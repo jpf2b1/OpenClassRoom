3ca2bfa158974f1c2351280400d0ce0f91fce7e5
fernandez <jpf2b1@gmail.com>
2019-10-11 09:53:20 +0200

    Ajout des fonction index, view et viewSlug dans le controller AdvertController

--- mooc-symfony4/src/Controller/AdvertController.php
+++ mooc-symfony4/src/Controller/AdvertController.php
@@ -5,34 +5,51 @@ namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class AdvertController extends Controller{
+class AdvertController extends AbstractController{
+
     /**
-     * Affiche "Bonjour Hello World"
+     * Affiche page index
      * 
-     * @Route("/", name="homepage")
-     * @Route("/hello-world")
+     * @Route("/advert", name="oc_advert_index1")
+     * @Route("/", name="oc_advert_index")
      *
      * @return void
      */
     public function index(){
-
-        //return new Response("Bonjour Hello World");
         return $this->render(
-            "Advert/index.html.twig",
-            ['title'=>'Bonjour a tous']
+            'Advert/index.html.twig', 
+            ['title'=>'ToTo Asticot']
         );
-
     }
 
+
+
     /**
-     * Undocumented function
+     * Affiche une page avec le numero saisi dans l'url
      * 
-     * @Route("/advert/view/{id}", name="oc_advert_view")
+     * @Route("/advert/index/{id}",name="oc_advert_view")
      *
      * @return void
      */
-    public function view($id='none'){
-        return new Response("Affichage de l'annonce d'id : ".$id);
+    public function view($id = "Non renseigné"){
+        return new Response("<body>Affichage de l'annonce numero : ".$id."</body>");
     }
+
+ 
+    /**
+     * @Route("/advert/view/{year}/{slug}.{format}", name="oc_advert_view_slug")
+     */
+    public function viewSlug($slug, $year, $format)
+    {
+    return new Response(
+        "On pourrait afficher l'annonce correspondant au
+        slug '".$slug."', créée en ".$year." et au format ".$format."."
+    );
+    }
+
+
+
+
 }
